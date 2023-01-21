553c904b21aa8ab0f12dd91501704b4d136aa7b1
yazayalks <lysak_kirill@mail.ru>
2023-01-21 18:21:26 +0700

    add NextNProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ThemeProvider, useTheme} from 'next-themes'
 import {ToastContainer} from "react-toastify";
-
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }: AppProps) {
 
 
   return ( <>
       <ThemeProvider>
+          <NextNProgress color="var(--icon-color-hover)" />
         <Component {...pageProps} />
           <ToastContainer
               position="top-right"
