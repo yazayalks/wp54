02a2576967a7aad3a7c27841d562be96277fc507
yazayalks <lysak_kirill@mail.ru>
2023-01-24 21:42:54 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,12 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {ThemeProvider} from 'next-themes'
 import {ToastContainer} from "react-toastify";
-<<<<<<< HEAD
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider>
+            <NextNProgress color="var(--icon-color-hover)"/>
             <Component {...pageProps} />
             <ToastContainer
                 position="top-right"
@@ -18,26 +19,4 @@ export default function App({Component, pageProps}: AppProps) {
         </ThemeProvider>
     )
 }
-=======
-import NextNProgress from 'nextjs-progressbar';
 
-export default function App({ Component, pageProps }: AppProps) {
-
-
-  return ( <>
-      <ThemeProvider>
-          <NextNProgress color="var(--icon-color-hover)" />
-        <Component {...pageProps} />
-          <ToastContainer
-              position="top-right"
-              hideProgressBar={false}
-              closeOnClick
-              rtl={false}
-          />
-      </ThemeProvider>
-
-      </>
-
-  )
-}
->>>>>>> main
