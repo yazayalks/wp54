80dc1020a06e5df7622384781107ae5150f9fe71
Kirill Lysak <lysak_kirill@mail.ru>
2023-10-21 00:34:48 +0700

    Fixed backgroundAnimation styles

--- pages/balls/index.tsx
+++ pages/balls/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import styles from "../../styles/backgroundAnimation.module.css";
+import styles from "../../styles/backgroundAnimation.module.scss";
 
 
 const Index = () => {
